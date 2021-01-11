e8d2e3f1416b61188b1d56d7ea7a3149ac0de37f
Polyana L. Pimenta <polyana.pimenta@gmail.com>
2021-01-11 17:10:19 -0300

    Updates

---
+++ precache-manifest.c71e3ce5cbee44d2601447712d3f4699.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370c37ff8dd5d27ba4626a96ee7eb27e",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "2c700137a93273baafc4",
+    "url": "/portifolio/static/css/2.05bd92c4.chunk.css"
+  },
+  {
+    "revision": "8de01d4977b1e7adba6b",
+    "url": "/portifolio/static/css/main.35e9dc87.chunk.css"
+  },
+  {
+    "revision": "2c700137a93273baafc4",
+    "url": "/portifolio/static/js/2.bb4a5f7f.chunk.js"
+  },
+  {
+    "revision": "2dc0d645f23eeb75525424ecacde1a6f",
+    "url": "/portifolio/static/js/2.bb4a5f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de01d4977b1e7adba6b",
+    "url": "/portifolio/static/js/main.063bdfd3.chunk.js"
+  },
+  {
+    "revision": "30ac0313c5b704790724",
+    "url": "/portifolio/static/js/runtime-main.4a3ca979.js"
+  },
+  {
+    "revision": "15363ffed10cdd8b28dd21216b9bf482",
+    "url": "/portifolio/static/media/background_en.15363ffe.png"
+  },
+  {
+    "revision": "b44f89ca6a0b616552705aec5bb2ced0",
+    "url": "/portifolio/static/media/background_pt.b44f89ca.png"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/portifolio/static/media/brand-icons.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/portifolio/static/media/brand-icons.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/portifolio/static/media/brand-icons.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/portifolio/static/media/brand-icons.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/portifolio/static/media/brand-icons.ec3cfdde.woff"
+  },
+  {
+    "revision": "0d7907f437e3f4ec22e4474cffc01672",
+    "url": "/portifolio/static/media/email.0d7907f4.svg"
+  },
+  {
+    "revision": "89e4d7ad0e550519ef9a7cd7e5ac831c",
+    "url": "/portifolio/static/media/emoji_polyana_notebook.89e4d7ad.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portifolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0c0d0a7bc23460d052f6b87639c86d46",
+    "url": "/portifolio/static/media/github.0c0d0a7b.svg"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/portifolio/static/media/icons.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/portifolio/static/media/icons.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/portifolio/static/media/icons.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/portifolio/static/media/icons.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/portifolio/static/media/icons.bea989e8.woff"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/portifolio/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "7c9810df320391e9552c48c3e7a3ac6d",
+    "url": "/portifolio/static/media/maps.7c9810df.svg"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/portifolio/static/media/outline-icons.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/portifolio/static/media/outline-icons.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/portifolio/static/media/outline-icons.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/portifolio/static/media/outline-icons.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/portifolio/static/media/outline-icons.f89ea91e.woff"
+  },
+  {
+    "revision": "38afd1891144a6d63f5955baafc615d3",
+    "url": "/portifolio/static/media/polyanapimenta.38afd189.png"
+  },
+  {
+    "revision": "74b1759224d1a36c4f4f33d3953683f8",
+    "url": "/portifolio/static/media/premiacao-2018.74b17592.gif"
+  },
+  {
+    "revision": "047028309e4287567a98984c5fa03b24",
+    "url": "/portifolio/static/media/premiacao-2019.04702830.gif"
+  },
+  {
+    "revision": "1b87589123e6f13589802703b9d5dc2a",
+    "url": "/portifolio/static/media/print.1b875891.svg"
+  },
+  {
+    "revision": "c2b96e812923b1ed29b119bafcf37623",
+    "url": "/portifolio/static/media/triangle.c2b96e81.svg"
+  },
+  {
+    "revision": "d2bf24b1e5d8e323cb24da78ce9a0aa1",
+    "url": "/portifolio/static/media/whatsapp.d2bf24b1.svg"
+  }
+]);
